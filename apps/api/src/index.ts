43fd401552ad00961b199c10252f0a2de4505feb
Tisha Murvihill <tmurv@shaw.ca>
2024-01-20 12:14:21 -0700

    add prettier

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,20 +1,20 @@
-import cors from 'cors'
-import express from 'express'
+import cors from "cors";
+import express from "express";
 
-import { Workspace } from 'types'
+import { Workspace } from "@timesheets/types";
 
-const app = express()
-const port = 5050
+const app = express();
+const port = 5050;
 
-app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors({ origin: "http://localhost:3000" }));
 
-app.get('/workspaces', (_, response) => {
-    const workspaces: Workspace[] = [
-        { name: 'api', version: '1.0.0' },
-        { name: 'types', version: '1.0.0' },
-        { name: 'web', version: '1.0.0' },
-    ]
-    response.json({ data: workspaces })
-})
+app.get("/workspaces", (_, response) => {
+  const workspaces: Workspace[] = [
+    { name: "timesheets API", version: "1.0.0" },
+    { name: "timesheets TYPES", version: "1.0.0" },
+    { name: "timesheets UI", version: "1.0.0" },
+  ];
+  response.json({ data: workspaces });
+});
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
