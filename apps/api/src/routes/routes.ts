e4dc6190fc0e8bf9b38687f3bf5602b683136382
Tisha Murvihill <tmurv@shaw.ca>
2024-02-10 09:17:57 -0700

    zod user signup from postman works

--- apps/api/src/routes/routes.ts
+++ apps/api/src/routes/routes.ts
@@ -1,14 +1,51 @@
 import passport from "passport";
 import { Router } from "express";
 import { v4 as uuid } from "uuid";
+import { z } from "zod";
 
 import { db } from "../config/database.js";
 import { generatePassword } from "../utils/passwords.js";
 import { User } from "../types/User.js";
 
 const router = Router();
+const BodySchema = User.pick({
+  first: true,
+  last: true,
+  email: true,
+  password: true,
+  phone: true,
+  role: true,
+  username: true,
+});
+
+const getText = (found: User) => {
+  if (found.email && found.phone) return "phone and email";
+  if (found.email) return "email";
+  if (found.phone) return "phone";
+};
 
 router.put("/signup", async (req, res) => {
+  const result = BodySchema.safeParse(req.body);
+  console.log("resultofparse", result);
+
+  if (!result.success) {
+    return res.status(400).send(result.error);
+  }
+
+  try {
+    const found = await db.collection("users").findOne({
+      $or: [{ email: req.body.email }, { phone: req.body.phone }],
+    });
+
+    if (found) {
+      return res
+        .status(400)
+        .send(`User with that ${getText(found)} already exists`);
+    }
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+
   const saltHash = generatePassword(req.body.password);
   const salt = saltHash.salt;
   const hash = saltHash.hash;
@@ -27,19 +64,33 @@ router.put("/signup", async (req, res) => {
     username: req.body.username,
   };
 
-  await db
-    .collection("users")
-    .updateOne({ id: newUser.id }, { $set: newUser }, { upsert: true });
+  try {
+    await db
+      .collection("users")
+      .updateOne({ id: newUser.id }, { $set: newUser }, { upsert: true });
+  } catch (error) {
+    // if (JSON.stringify(error).includes("11000")) {
+    //   return res.redirect("/auth/signup-failure/11000");
+    // }
+    // return res.redirect("/auth/signup-failure");
+    res
+      .status(500)
+      .send(
+        JSON.stringify(error).includes("11000")
+          ? "Email or phone already exists."
+          : error,
+      );
+  }
 
   res.redirect("/auth/login");
 });
 
 router.post(
-    "/login",
-    passport.authenticate("local", {
-      failureRedirect: "/auth/login-failure",
-      successRedirect: "/auth/login-success",
-    }),
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/auth/login-failure",
+    successRedirect: "/auth/login-success",
+  }),
 );
 
 router.get("/login-success", (req, res) => {
@@ -48,6 +99,14 @@ router.get("/login-success", (req, res) => {
 
 router.get("/login-failure", (req, res) => {
   res.send("Login not successful");
+});
+
+router.get("/signup-failure/11000", (req, res) => {
+  res.send("An account with that email or phone number already exists");
+});
+
+router.get("/signup-failure/11000", (req, res) => {
+  res.send("Signup not successful");
 });
 
 router.get("/login", (req, res) => {
--- apps/api/src/types/User.ts
+++ apps/api/src/types/User.ts
@@ -8,22 +8,28 @@
 
 import { z } from "zod";
 
-const User = z.object({
+export const User = z.object({
   createdOn: z.string().datetime().min(1),
-  email: z.string().email(),
-  first: z.string(),
+  email: z.string().email().optional(),
+  first: z.string().min(1),
   id: z.string().min(1),
   last: z.string().min(1),
-  password: z.string().min(1),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" })
+    .regex(/[a-z]/, { message: "Password must contain a lowercase letter" })
+    .regex(/[A-Z]/, { message: "Password must contain an uppercase letter" })
+    .regex(/[0-9]/, { message: "Password must contain a number" }),
+
   preferredContact: z.enum(["email", "phone"]),
-  phone: z.string(),
+  phone: z.string().optional(),
   role: z.enum(["user", "admin"]).default("user"),
   salt: z.string(),
-  username: z.string(),
+  username: z.string().optional(),
 });
 
 // User.parse({ username: "Ludwig" });
 
 // extract the inferred type
-export type User = z.infer<typeof User>;
+// export type User = z.infer<typeof User>;
 // { username: string }
